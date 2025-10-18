8d5bc19d77cccd3242452df14954610582544543
Sweranzo <jeffcarandang5@gmail.com>
2025-10-18 15:13:21 +0800

    project done
    project done

--- index.js
+++ index.js
@@ -3,35 +3,66 @@ function addNumbers (a,b) {
   return total;
 }
 
-console.log(addNumbers(5,5));
+
 
 function subtractNumbers (a,b) {
   total = a-b;
   return total;
 }
 
-console.log(subtractNumbers(10,5));
 
 function multiplyNumbers (a,b) {
   total = a * b; 
   return total;
 }
 
-console.log(multiplyNumbers(5,5));
 
 
 function divideNumbers(a,b) {
+  if (a === 0 && b === 0) {
+    alert('You cant divide this shit');
+    inputBox.value = '';
+  }
   total = a / b;
   return total;
 }
 
-console.log(divideNumbers(5,10));
 
 
 const inputBox = document.querySelector ('.inputNumber');
 const buttons = document.querySelectorAll('.calculatorButtons');
 
 
-buttons.addEventListener('click', () => {
-  console.log(buttons.value);
-})
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    if(btn.textContent === '=') {
+     const [firstInput, sign, secondInput] = inputBox.value.split(/([+\-*/])/);
+     let total = operate(Number(firstInput), sign,Number(secondInput));
+      inputBox.value = total;
+      console.log([firstInput, sign, secondInput]);
+    } else if(btn.textContent === 'Clr') {
+      inputBox.value = '';
+    }else if (btn.textContent === '←') {
+      inputBox.value = inputBox.value.slice(0,-1);
+    }else{
+  inputBox.value += btn.textContent; 
+    }
+
+  });
+});
+
+
+function operate(firstNumber,operator,secondNumber){ 
+  let total = '';
+  if (operator === '+') { 
+    total = addNumbers(firstNumber,secondNumber);
+  } else if (operator === '-') {
+    total = subtractNumbers(firstNumber,secondNumber);
+  } else if (operator === '*') {
+    total = multiplyNumbers(firstNumber,secondNumber);
+  } else if (operator === '/') {
+    total = divideNumbers(firstNumber,secondNumber);
+  }
+
+  return total; 
+}
